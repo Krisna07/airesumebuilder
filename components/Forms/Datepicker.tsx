457be9da5a5d13e42695782276a2b9dbe52e11fa
krisna07 <krisnachhetri07@gmail.com>
2025-09-19 21:28:04 +1000

    adding some changes on calendar

--- components/Forms/Datepicker.tsx
+++ components/Forms/Datepicker.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import { Calendar } from 'lucide-react';
+import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 
 interface DatePickerProps {
@@ -11,31 +14,44 @@ interface DatePickerProps {
 
 export default function Datepicker({ value, index, target, update }: DatePickerProps) {
   const isValidDate = value ? !isNaN(new Date(value).getTime()) : false;
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleChange = (date: Date | null) => {
+    if (date) {
+      const options = {
+        month: 'short' as const,
+        year: 'numeric' as const
+      };
+      const formattedDate = date.toLocaleDateString('en-US', options);
+      update(index, target, formattedDate); // Update the date in the state
+      setOpen(false);
+    }
+  };
 
   return (
-    <label className='w-full grid gap-1 transition-all ease-in-out text-[14px] font-sans relative'>
-      {target == 'startDate' ? 'Start Date' : 'End Date'}
-      <div className='w-full bg-white  outline-none  ring-1 ring-gray-200 focus:ring-green-600 transition-all ease-in-out duration-300 px-[8px] py-[4px] text-[14px] rounded-md relative z-[100] flex items-center'>
-        <DatePicker
-          selected={isValidDate ? new Date(value!) : null}
-          onChange={(date: Date | null) => {
-            if (date) {
-              const options = {
-                month: 'short' as const,
-                year: 'numeric' as const
-              };
-              const formattedDate = date.toLocaleDateString('en-US', options);
-              update(index, target, formattedDate); // Update the date in the state
-            }
-          }}
-          showMonthYearPicker
-          placeholderText={value ? value : 'Select Date'}
-          dateFormat='yyyy-MM'
-          className=' w-full outline-none '
-        />
+    <label className='w-full grid gap-1  transition-all ease-in-out text-[14px] font-sans relative z-[20]'>
+      {target === 'startDate' ? 'Start Date' : 'End Date'}
+      <div className='w-full  outline-none ring-1 ring-gray-200 focus-within:ring-green-600 transition-all ease-in-out duration-300 px-[8px] py-[4px] text-[14px] rounded-md relative flex items-center gap-2'>
         <span>
           <Calendar className='w-4 h-4' color={'gray'} />
         </span>
+
+        <DatePicker
+          selected={isValidDate ? new Date(value!) : null}
+          onChange={handleChange}
+          open={open}
+          onClickOutside={() => setOpen(false)}
+          shouldCloseOnSelect={true}
+          showMonthYearPicker
+          placeholderText={value ? value : 'Select Date'}
+          dateFormat='yyyy-MM'
+          maxDate={new Date()}
+          className='react-datepicker-hidden-input'
+          calendarClassName='rd-calendar animate-pop'
+          showPopperArrow={true}
+          popperPlacement='bottom-end'
+        />
+        <div onClick={() => setOpen(!open)} className='w-full h-full absolute bg-transparent'></div>
       </div>
     </label>
   );
