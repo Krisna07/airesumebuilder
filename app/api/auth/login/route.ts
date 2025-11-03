10dab33fbb1bb626b604b1d63300712e874fc2d0
Paudel <krisnachhetri07@gmail.com>
2025-11-03 20:47:15 +1100

    adding middleware

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -7,21 +7,25 @@ const prisma = new PrismaClient();
 export async function POST(req: NextRequest) {
   try {
     const { email, password } = await req.json();
+
     if (!email || !password) {
       return NextResponse.json(
         { error: 'Email and password are required.' },
         { status: 400 }
       );
     }
+
     const user = await prisma.user.findUnique({ where: { email } });
+
     if (!user) {
       return NextResponse.json(
         { error: 'User not found, please register.' },
         { status: 401 }
       );
     }
+
     const dbPassword = user.password;
-    if(!dbPassword) {
+    if (!dbPassword) {
       return NextResponse.json(
         { error: 'User has no password set, please use OAuth login.' },
         { status: 401 }
@@ -35,16 +39,16 @@ export async function POST(req: NextRequest) {
         { status: 401 }
       );
     }
+
     return NextResponse.json(
       { data: { id: user.id, email: user.email, name: user.name } },
       { status: 200 }
     );
   } catch (error) {
-    return NextResponse.json({
-      error: JSON.stringify(error)
-    },
-      {
-        status: 500
-    });
+    console.error('Login error:', error);
+    return NextResponse.json(
+      { error: 'Internal server error' },
+      { status: 500 }
+    );
   }
 }
