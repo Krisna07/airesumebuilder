97db1b53937399d97188b2fd4c8232e77f0d7bed
krisna07 <krisnachhetri07@gmail.com>
2026-02-16 23:45:25 +1100

    updated the gmail service

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -7,9 +7,7 @@ import { validate } from "deep-email-validator";
 
 
 export const createTransporter = (auth: { user: string, pass: string }) => nodemailer.createTransport({
-    host: "smtp.zoho.com.au",
-    port: 465,
-    secure: true, // Use true for port 465, false for port 587
+    service: 'gmail',// Use true for port 465, false for port 587
     auth: auth
 });
 
